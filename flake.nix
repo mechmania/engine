346b3cf97af5ba46eea6422e3ff252302ebe020b
roshan <roshanahegde@gmail.com>
2025-06-07 13:19:10 -0500

    batman

---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  description = "tinker with configuration files without having to rebuild your system";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , ...
+    } @ inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ (import inputs.rust-overlay) ];
+      };
+
+      craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+        p:
+        p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+      );
+
+      # Common arguments can be set here to avoid repeating them later
+      # Note: changes here will rebuild all dependency crates
+      commonArgs = {
+        src = craneLib.cleanCargoSource ./.;
+        strictDeps = true;
+
+        buildInputs =
+          [
+            # Add additional build inputs here
+          ]
+          ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+      };
+
+      bin = craneLib.buildPackage (commonArgs
+        // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Additional environment variables or build phases/hooks can be set
+        # here *without* rebuilding all dependency crates
+        # MY_CUSTOM_VAR = "some value";
+      });
+    in
+    {
+      checks = {
+
+        inherit bin;
+      };
+      packages.default = bin;
+      apps.default = inputs.flake-utils.lib.mkApp {
+        drv = bin;
+      };
+      devShells.default = craneLib.devShell {
+        checks = self.checks.${system};
+        packages = [
+          (pkgs.python3.withPackages (ps:
+            with ps; [
+              pygame
+              toml
+            ]))
+        ];
+      };
+    });
+}
